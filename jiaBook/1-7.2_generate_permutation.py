336aad6f457a9f230b0a85b219c63e29673b7dd7
Zhiyi Luo <jessieluo1991@gmail.com>
2016-03-27 14:14:45 +0800

    1-7.2 duplicate situation

--- jiaBook/1-7.2_generate_permutation.py
+++ jiaBook/1-7.2_generate_permutation.py
@@ -3,15 +3,16 @@ def generatePermutation(prefix,S):
     if len(S) == 0: print prefix
     else:
         for i in range(len(S)):
+	    if i>0 and S[i]==S[i-1]: continue # add this line to deal with the duplicate elements
             cur = S[i]
-	    if cur not in prefix:	
-		S.pop(i)
-	        generatePermutation(prefix+[cur], S)
-		S.insert(i,cur)
+            S.pop(i)
+	    generatePermutation(prefix+[cur], S)
+	    S.insert(i,cur)
 
-def perm(n):
-    S = [i+1 for i in range(n)]
+def perm(S=[],n=1):
+    if not S: S = [i+1 for i in range(n)]
     generatePermutation([],S)
 
-perm(3)
-perm(4)
+perm(n=3)
+perm(n=4)
+perm(S=[1,1,2])
