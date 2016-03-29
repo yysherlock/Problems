cbc19cb3c7291f9042b78ed6b8c4bf79d8fd8ef3
Zhiyi Luo <jessieluo1991@gmail.com>
2016-03-29 23:34:11 +0800

    add hardstring

---
+++ jiaBook/1-7.4_backtrack_hardstring.py
@@ -0,0 +1,39 @@
+global cnt
+
+def search(S,sol):
+    global cnt
+
+    #"""    
+    if sol: print ''.join(sol)
+    if cnt == N: return 0
+    else: cnt += 1
+    """
+    if cnt == N:
+	print ''.join(sol)
+	return 0
+    else: cnt += 1
+    """
+
+    for i in range(len(S)):
+	cur = S[i]
+	sol.append(cur)
+        ok = 1
+	for j in range(len(sol)):
+	    if 2*j>=len(sol) and sol[j:] == sol[j-(len(sol)-j):j]: 
+	        ok = 0
+		break
+	if ok:
+	    if not search(S,sol): return 0
+	sol.pop(-1)
+        
+    return 1
+
+#S = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O',\
+#	'P','Q','R','S','T','U','V','W','X','Y','Z']
+
+L = 3 # use first L characters
+N = 7 # find Nth hard string
+cnt = 0
+
+search(['A','B','C'],[])
+
