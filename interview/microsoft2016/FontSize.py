6ec0af2f14534fbca19f49a453fae7a306daa502
Zhiyi Luo <jessieluo1991@gmail.com>
2016-04-06 21:38:12 +0800

    microsoft 2016

--- interview/microsoft2016/FontSize.py
+++ interview/microsoft2016/FontSize.py
@@ -7,11 +7,10 @@ while case < t:
     n,p,w,h = (int(x) for x in raw_input().split())
     a = [int(x) for x in raw_input().split()]
     result = w
-
+    
     for i in range(w): # i:0~w-1, s: w~1
         s = w - i
         maxlines = p * (h/s)
-        #print 's:',s,'maxlines:',maxlines
         curlines = 0
         found = 1
         for j in range(len(a)): # jth para.
@@ -19,9 +18,6 @@ while case < t:
             if maxlines==0 or curlines > maxlines:
                 found = 0
                 break
-            #print 'w:',w,'s:',s,"w/s:",w/s,'a[j]:',a[j]
-            #print 'math.ceil(float(a[j])/(w/s)):',math.ceil(float(a[j])/(w/s))
-
         if found:
             print s
             break
