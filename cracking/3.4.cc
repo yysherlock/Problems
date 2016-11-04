6036ccc55c253b4fcc7cc28606e4712e7a386a65
yysherlock <jessieluo1991@gmail.com>
2016-11-04 23:50:57 +0800

    hanoi

--- cracking/3.4.cc
+++ cracking/3.4.cc
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <stack>
 using namespace std;
 
 void hanoi(int n, char src, char bri, char dst){
@@ -13,9 +14,37 @@ void hanoi(int n, char src, char bri, char dst){
 
 struct op {
     int begin, end;
-    
+    char src, bri, dst;
+    op(){
+
+    }
+    op(int pbegin, int pend, char psrc, char pbri, char pdst):
+        begin(pbegin),end(pend),src(psrc),bri(pbri),dst(pdst){
+
+        }
+};
+
+void hanoi1(int n, char src, char bri, char dst){
+    stack<op> st;
+    op tmp;
+    st.push(op(1, n, src, bri, dst));
+    while(!st.empty()){
+        tmp = st.top();
+        st.pop();
+        if (tmp.begin != tmp.end){
+            st.push(op(tmp.begin, tmp.end-1, tmp.bri, tmp.src, tmp.dst));
+            //st.push(tmp);
+            st.push(op(tmp.end, tmp.end, tmp.src, tmp.bri, tmp.dst));
+            st.push(op(tmp.begin, tmp.end-1, tmp.src, tmp.dst, tmp.bri));
+        } else {
+            cout << "plate " << tmp.begin << ": " << tmp.src << "->" << tmp.dst << endl;
+        }
+    }
 }
+
 int main(){
     hanoi(3, 'A', 'B', 'C');
+    cout << endl;
+    hanoi1(3, 'A', 'B', 'C');
     return 0;
 }
